3fb09c3f50cf707e6fc41a3678dba32db10d377d
GVindio <97354117+GVindio@users.noreply.github.com>
2022-03-28 23:14:51 -0400

    First Commit - Hybrid Framework

---
+++ Hybrid_Framework/src/test/java/test/AddCustomerTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.AddCustomerPage;
+import page.DashboardPage;
+import page.LoginPage;
+import util.BrowserFactory;
+
+public class AddCustomerTest  {
+	
+	 
+
+ 	WebDriver driver;
+	
+ 	@Test
+ 	public void validUserShouldBeAbleToAddCustomer() {
+		
+		driver= BrowserFactory.init();
+		
+		LoginPage loginPage =  PageFactory.initElements(driver, LoginPage.class);
+		loginPage.insertUserName("demo@techfios.com");
+		loginPage.insertPassword("abc123");
+		loginPage.clikSigninButton();
+		
+		DashboardPage dashboardPage = PageFactory.initElements(driver, DashboardPage.class);
+		dashboardPage.verifyDashboardElement();
+		dashboardPage.clickCustomerButton(); 
+		dashboardPage.clickAddCustomerButton();
+		
+		AddCustomerPage addCustomerPage = PageFactory.initElements(driver, AddCustomerPage.class);
+		addCustomerPage.enterFullName("Selenium");
+		addCustomerPage.selectCompanyDropdown("Techfios");
+		addCustomerPage.enterEmail("abc@techfios.com");
+		
+		BrowserFactory.tearDown();
+		
+ 	} 
+}
+
